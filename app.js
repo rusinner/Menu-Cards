3347ade0cddc1059a031cd347171b95eaca974a7
rusinner <rusinner88@gmail.com>
2022-01-17 14:11:11 +0200

    practice-branch

--- app.js
+++ app.js
@@ -1,80 +1,80 @@
-const menu = [
+const products = [
     {
       id: 1,
-      title: "buttermilk pancakes",
-      category: "breakfast",
+      title: "HP 400 g3",
+      category: "desktop",
       price: 15.99,
-      img: "./images/item-1.jpeg",
+      img: "./images/400g25front.png",
       desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
     },
     {
       id: 2,
-      title: "diner double",
-      category: "lunch",
+      title: "Dell E6470",
+      category: "laptop",
       price: 13.99,
       img: "./images/item-2.jpeg",
       desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
     },
     {
       id: 3,
-      title: "godzilla milkshake",
-      category: "shakes",
+      title: "Samsung Galaxy",
+      category: "smartphones",
       price: 6.99,
       img: "./images/item-3.jpeg",
       desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
     },
     {
       id: 4,
-      title: "country delight",
-      category: "breakfast",
+      title: "Dell 9020",
+      category: "desktop",
       price: 20.99,
       img: "./images/item-4.jpeg",
       desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
     },
     {
       id: 5,
-      title: "egg attack",
-      category: "lunch",
+      title: "HP Pavillion",
+      category: "laptop",
       price: 22.99,
       img: "./images/item-5.jpeg",
       desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
     },
     {
       id: 6,
-      title: "oreo dream",
-      category: "shakes",
+      title: "iPhone 11",
+      category: "smartphones",
       price: 18.99,
       img: "./images/item-6.jpeg",
       desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
     },
     {
       id: 7,
-      title: "bacon overflow",
-      category: "breakfast",
+      title: "Fujitsu Esprimo",
+      category: "desktop",
       price: 8.99,
       img: "./images/item-7.jpeg",
       desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
     },
     {
       id: 8,
-      title: "american classic",
-      category: "lunch",
+      title: "Toshiba Satellite",
+      category: "laptop",
       price: 12.99,
       img: "./images/item-8.jpeg",
       desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
     },
     {
       id: 9,
-      title: "quarantine buddy",
-      category: "shakes",
+      title: "Huawei P40 Pro",
+      category: "smartphones",
       price: 16.99,
       img: "./images/item-9.jpeg",
       desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
     },
     {
       id: 10,
-      title: "bison steak",
-      category: "dinner",
+      title: "HP ProLiant G8",
+      category: "servers",
       price: 22.99,
       img: "./images/item-10.jpeg",
       desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
@@ -86,7 +86,7 @@ const menu = [
   const btnContainer = document.querySelector(".btn-container");
   // display all items when page loads
   window.addEventListener("DOMContentLoaded", function () {
-    diplayMenuItems(menu);
+    diplayMenuItems(products);
     displayMenuButtons();
   });
   
@@ -113,7 +113,7 @@ const menu = [
     sectionCenter.innerHTML = displayMenu;
   }
   function displayMenuButtons() {
-    const categories = menu.reduce(
+    const categories = products.reduce(
       function (values, item) {
         if (!values.includes(item.category)) {
           values.push(item.category);
@@ -138,14 +138,14 @@ const menu = [
       btn.addEventListener("click", function (e) {
         // console.log(e.currentTarget.dataset);
         const category = e.currentTarget.dataset.id;
-        const menuCategory = menu.filter(function (menuItem) {
+        const menuCategory = products.filter(function (menuItem) {
           // console.log(menuItem.category);
           if (menuItem.category === category) {
             return menuItem;
           }
         });
         if (category === "all") {
-          diplayMenuItems(menu);
+          diplayMenuItems(products);
         } else {
           diplayMenuItems(menuCategory);
         }
